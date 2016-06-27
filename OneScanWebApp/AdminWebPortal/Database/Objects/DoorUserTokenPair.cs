a52b49a11a9bec42d7680cb2334fea6f04d7b0b6
UHSL-Marcus <m.lee23@herts.ac.uk>
2016-06-27 14:52:32 +0100

    added ID column attribute

--- OneScanWebApp/AdminWebPortal/Database/Objects/DoorUserTokenPair.cs
+++ OneScanWebApp/AdminWebPortal/Database/Objects/DoorUserTokenPair.cs
@@ -4,6 +4,8 @@ namespace AdminWebPortal.Database.Objects
 {
     public class DoorUserTokenPair : DatabaseTableObject
     {
+        [DatabaseID]
+        public int? Id;
         public int? DoorID;
         public int? UserToken;
     }
